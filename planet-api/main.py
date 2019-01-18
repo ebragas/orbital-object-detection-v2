0145ec4b9e10034146da544631494ac08089ca93
Eric Bragas <ericbragas412@gmail.com>
2019-01-18 15:36:42 -0800

    module test

---
+++ planet-api/main.py
@@ -0,0 +1,8 @@
+from planet.planet import hello_world
+
+
+def run():
+    hello_world()
+
+if __name__ == "__main__":
+    run()
---
+++ planet-api/planet/planet.py
@@ -0,0 +1,6 @@
+"""Wrapper for Planet API"""
+
+# TODO: OOP
+def hello_world(name=None):
+    """Hello World test function"""
+    print 'Hello, {}!'.format(name or "World")
